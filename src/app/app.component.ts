55c741b58bd0d7b07db5736f4e77930ed6e6562c
radia idelkadi <idelkadiradia@gmail.com>
2024-12-30 19:29:04 +0100

    MEL-2 Create component & the aside menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AsideMenuComponent } from './components/aside-menu/aside-menu.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet ,AsideMenuComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
